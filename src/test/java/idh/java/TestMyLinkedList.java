e0d6b5ffd5b9562e513b2d7aa71598917fcd2d2a
Jürgen Hermes <hermesj@uni-koeln.de>
2025-07-09 10:46:35 +0200

    Formatted

--- src/test/java/idh/java/TestMyLinkedList.java
+++ src/test/java/idh/java/TestMyLinkedList.java
@@ -1,6 +1,5 @@
 package idh.java;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,42 +12,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestMyLinkedList {
-    MyLinkedList<String> list;
+	MyLinkedList<String> list;
 
-    @BeforeAll
-    public static void setUpOnce() {
+	@BeforeAll
+	public static void setUpOnce() {
 
-    }
+	}
 
-    @BeforeEach
-    public void setUp() {
-	list = new MyLinkedList<String>();
-    }
+	@BeforeEach
+	public void setUp() {
+		list = new MyLinkedList<String>();
+	}
 
-    @Test
-    public void testEmptyList() {
-	assertEquals(0, list.size());
-    }
+	@Test
+	public void testEmptyList() {
+		assertEquals(0, list.size());
+	}
 
-    @Test
-    public void testIsEmpty() {
-	assertTrue(list.isEmpty());
-	assertFalse(!list.isEmpty());
-    }
-    
-    @Test
-    public void testException() {
-	
-	assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
-    }
+	@Test
+	public void testIsEmpty() {
+		assertTrue(list.isEmpty());
+		assertFalse(!list.isEmpty());
+	}
 
-    @AfterAll
-    public static void afterall() {
-	
-    }
-    
-    @AfterEach
-    public void aftereach() {
+	@Test
+	public void testException() {
+		assertThrows(IndexOutOfBoundsException.class, () -> list.get(5));
+	}
 
-    }
+	@AfterAll
+	public static void afterall() {
+
+	}
+
+	@AfterEach
+	public void aftereach() {
+
+	}
 }
